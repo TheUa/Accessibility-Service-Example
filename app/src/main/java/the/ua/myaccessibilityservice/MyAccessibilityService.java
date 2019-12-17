ac4524263e9e5016508b9d7cc2376149207f4207
TheUa <the.dn.ua@gmail.com>
2019-12-17 08:27:21 +0200

    Initial

--- app/src/main/java/the/ua/myaccessibilityservice/MyAccessibilityService.java
+++ app/src/main/java/the/ua/myaccessibilityservice/MyAccessibilityService.java
@@ -4,9 +4,15 @@ import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
+
+import java.util.List;
 
 public class MyAccessibilityService extends AccessibilityService {
     public MyAccessibilityService() {
@@ -30,6 +36,8 @@ public class MyAccessibilityService extends AccessibilityService {
                 return "TYPE_WINDOW_STATE_CHANGED";
             case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
                 return "TYPE_VIEW_TEXT_CHANGED";
+            case AccessibilityEvent.TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY:
+                return "TYPE_VIEW_TEXT_TRAVERSED_AT_MOVEMENT_GRANULARITY";
         }
         return "default";
     }
@@ -44,10 +52,13 @@ public class MyAccessibilityService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.e(TAG, String.format(
-                "onAccessibilityEvent: [type] %s [class] %s [package] %s [time] %s [text] %s",
+        Log.v(TAG, String.format(
+                "onAccessibilityEvent: [type] %s [class] %s [package] %s [time] %s [text] %s" ,
                 getEventType(event), event.getClassName(), event.getPackageName(),
                 event.getEventTime(), getEventText(event)));
+        getYouTubeSearch(event);
+
+
     }
 
     @Override
@@ -67,7 +78,45 @@ public class MyAccessibilityService extends AccessibilityService {
 
         info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+//        info.notificationTimeout = 100;
         setServiceInfo(info);
+
+    }
+
+    private void getYouTubeSearch(AccessibilityEvent event) {
+
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            if (event.getPackageName().equals("com.google.android.youtube")) {
+                AccessibilityNodeInfo currentNode = getRootInActiveWindow();
+                if (currentNode != null) {
+
+//                    List<AccessibilityNodeInfo> nodeInfoList = currentNode.findAccessibilityNodeInfosByText("Введите запрос");
+                    List<AccessibilityNodeInfo> nodeInfoList =
+                            currentNode.findAccessibilityNodeInfosByText("Введите запрос");
+
+                    if (nodeInfoList != null && !nodeInfoList.isEmpty()) {
+                        for (AccessibilityNodeInfo nodeInfo : nodeInfoList) {
+
+                            if (nodeInfo.getClassName().equals("android.widget.EditText")) {
+                                Log.e("Ed", (String) currentNode.getContentDescription());
+                            }
+
+                            Log.e("1111", String.valueOf(nodeInfo.getContentDescription()));
+                            Bundle arguments = new Bundle();
+                            nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                            arguments.putString(AccessibilityNodeInfoCompat.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, "Lol777");
+                            Log.e("2222", String.valueOf(nodeInfo));
+                            nodeInfo.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments);
+//                            nodeInfo.setText("fdfsdf");
+
+                            Log.e("1111", String.valueOf(nodeInfo.getClassName()));
+
+                        }
+
+                    }
+                }
+            }
+        }
     }
 
 }
